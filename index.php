550cde24a67856f11a0bfa7b4d6738b3ad2327a0
DoodleForce <random5678998765@gmail.com>
2025-04-14 18:29:47 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+	
+	<html>
+		<head>
+			</head>
+			<body>
+				<form action="login.php">
+				<input type=text; placeholder=Username>
+					<div>
+						<br>
+				<input type=text placeholder=Password>
+					<div>
+						<br>
+					<input type=submit>
+						<?php
+echo $_GET["username"]
